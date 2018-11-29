a5fbc37a09353ce7d94c0ec5b43d9a11e85bc49d
zhangchenxi <648510978@qq.com>
2018-11-29 18:12:16 +0800

    新建sonModule

---
+++ LoanProject/Classes/Certification/SonModule/IDCheckVC.h
@@ -0,0 +1,17 @@
+//
+//  IDCheckVC.h
+//  LoanProject
+//
+//  Created by 张晨曦 on 2018/11/29.
+//  Copyright © 2018年 张晨曦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IDCheckVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LoanProject/Classes/Certification/SonModule/IDCheckVC.m
@@ -0,0 +1,32 @@
+//
+//  IDCheckVC.m
+//  LoanProject
+//
+//  Created by 张晨曦 on 2018/11/29.
+//  Copyright © 2018年 张晨曦. All rights reserved.
+//
+
+#import "IDCheckVC.h"
+
+@interface IDCheckVC ()
+
+@end
+
+@implementation IDCheckVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
